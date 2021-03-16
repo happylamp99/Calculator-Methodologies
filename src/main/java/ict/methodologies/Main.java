a7deda0ff19093c3cfcf2ed0b494c8a3cbc8998f
happylamp99 <happylamp88@gmail.com>
2021-03-17 01:46:28 +0200

    Added History and Answer

--- src/main/java/ict/methodologies/Main.java
+++ src/main/java/ict/methodologies/Main.java
@@ -4,24 +4,34 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
-/**
- * JavaFX Main
- */
 public class Main extends Application {
+    private static Stage historyStage=null;
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/calcForm.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("calcForm.fxml"));
         stage.setTitle("Calc");
         stage.setScene(new Scene(root));
         stage.show();
+        createHistoryStage();
     }
-    
+
+    public void createHistoryStage(){
+        historyStage=new Stage();
+        historyStage.setTitle("Calculation History");
+        historyStage.setAlwaysOnTop(true);
+        historyStage.setResizable(false);
+        historyStage.initModality(Modality.APPLICATION_MODAL);
+    }
+
+    public static Stage getHistoryStage(){
+        return historyStage;
+    }
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/ict/methodologies/controllers/CalcController.java
+++ src/main/java/ict/methodologies/controllers/CalcController.java
@@ -1,9 +1,17 @@
 package ict.methodologies.controllers;
+
+import ict.methodologies.Main;
 import ict.methodologies.utils.EvaluateString;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class CalcController {
     @FXML
@@ -11,6 +19,8 @@ public class CalcController {
 
     @FXML
     private Label result;
+
+    private ArrayList<String> calc_history = new ArrayList<>();
 
     public void insertNumber(String number){
         expression.setText(expression.getText() + number);
@@ -26,6 +36,9 @@ public class CalcController {
         return expression;
     }
 
+    public void insertAnswer(String answer){
+        expression.setText(expression.getText()+ " " + answer);
+    }
     public void deleteLast(){
         if (!getExpression().getText().isEmpty()){
             StringBuilder text = new StringBuilder(getExpression().getText());
@@ -34,10 +47,30 @@ public class CalcController {
         }
     }
 
+    public Label getResult(){
+        return result;
+    }
     public  void setResult(String newResult){
         this.result.setText("= " + newResult);
     }
 
+    public void openHistoryWindow(){
+        try{
+            FXMLLoader loader=new FXMLLoader(getClass().getResource("/historyForm.fxml"));
+            Parent root = loader.load();
+
+            Main.getHistoryStage().setScene(new Scene(root));
+            HistoryController historyController=loader.getController();
+            historyController.initCalculations(calc_history);
+
+            Main.getHistoryStage().show();
+        }catch(IOException ex){
+            System.out.println(ex);
+        }
+    }
+    public void addCalculation(String expression, String result){
+        this.calc_history.add(expression+ " = "+ result);
+    }
     public void onMouseClick(MouseEvent mouseEvent){
         Button button = (Button) mouseEvent.getSource();
         String buttonText= button.getText();
@@ -66,16 +99,18 @@ public class CalcController {
                 break;
             case("="):
                  double result = EvaluateString.evaluate( this.getExpression().getText());
+                 addCalculation(this.getExpression().getText(),String .valueOf(result));
                  setResult(String.valueOf(result));
                  break;
             case("Del"):
                 deleteLast();
                 break;
-
-
-
-
-
+            case("ANS"):
+                insertAnswer(getResult().getText().substring(2));
+                break;
+            case("History"):
+                openHistoryWindow();
+                break;
         }
     }
 
